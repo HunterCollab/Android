774b461e056f68e9a88f80fbd204443d230f4033
Hugh <hcl212@users.noreply.github.com>
2019-03-18 03:23:03 -0400

    profile page UI

---
+++ app/src/main/java/com/example/socialmediaapp/ProfilePage.java
@@ -0,0 +1,20 @@
+package com.example.socialmediaapp;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfilePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_page);
+
+        // Show the Up button in the action bar.
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+}
