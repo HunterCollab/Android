1ed900728641719f04023a8790d957960500c74a
RamVakad <RamVakad@gmail.com>
2019-05-10 13:39:35 -0400

    Dev -> Prod

--- app/src/main/java/com/huntercollab/app/config/GlobalConfig.java
+++ app/src/main/java/com/huntercollab/app/config/GlobalConfig.java
@@ -8,8 +8,8 @@ package com.huntercollab.app.config;
 
 public class GlobalConfig {
 
-    //public static final String HOST = "13.58.204.157";
-    public static final String HOST = "10.0.2.2";
+    public static final String HOST = "13.58.204.157";
+    //public static final String HOST = "10.0.2.2";
     public static final int PORT = 5000;
     public static final int RMS_PORT = 8484;
     public static final String BASE_API_URL = "http://" + HOST + ":" + PORT;
